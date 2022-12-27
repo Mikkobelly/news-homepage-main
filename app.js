9ca0b5c5c03417972b90d9bf83d687c2f2a84712
mikia <mikiakuta0529@gmail.com>
2022-12-27 11:09:00 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const navButton = document.querySelector('.navigation__toggle-box');
+const navPopup = document.querySelector('.nav-popup-section');
+const popupContent = document.querySelector('.nav-popup');
+const closeButton = document.querySelector('.nav-popup__close');
+
+
+navButton.addEventListener("click", () => {
+    navPopup.style.opacity = 1;
+    navPopup.style.visibility = "visible";
+    popupContent.style.opacity = 1;
+    popupContent.style.visibility = "visible";
+
+})
+
+closeButton.addEventListener("click", () => {
+    navPopup.style.opacity = 0;
+    navPopup.style.visibility = "hidden";
+    popupContent.style.opacity = 0;
+    popupContent.style.visibility = "hidden";
+
+})
